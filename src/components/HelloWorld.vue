cb05357070861a9ba5ff3e01db610f6c7db7e8cf
mohsen moradkhani <maryamghashghaei60@gmail.com>
2023-06-09 12:53:19 +0200

    nasa api and store connected for fetching data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,10 @@
     <select v-model="selectedItem" v-on:change="handleChange">
       <option v-for="item in items" :value="item" :key="item">{{ item }}</option>
     </select>
- 
   </div>
 </template>
-
 <script>
 import { NasaApi } from './../services/NasaApi'
-
 export default {
   name: "HelloWorld",
   data() {
@@ -24,17 +21,28 @@ export default {
   async mounted() {
     this.selectedItem = this.items[0];
     this.currentPage = 1;
-   },
+    await this.getData();
+  },
   props: {
     msg: String,
   },
   methods: {
- 
     async handleChange() {
       this.currentPage = 1;
       this.nasaData = [];
-     },
- 
+      await this.getData();
+    },
+    async getData() {
+      const cachedData = await this.$store.dispatch('getCachedData', { key: this.selectedItem, page: this.currentPage });
+      if (cachedData) {
+        this.nasaData = cachedData;
+      } else {
+        let res = await NasaApi.getNasaMarchData(this.selectedItem, this.currentPage);
+        let tosave = res.photos.map((c) => ({ id: c.id, earth_date: c.earth_date, img_src: c.img_src }));
+        this.$store.commit('setCachedData', { key: this.selectedItem, page: this.currentPage, data: this.tosave });
+        this.nasaData = [...this.nasaData, ...tosave]
+      }
+    }
   }
 };
 </script>
